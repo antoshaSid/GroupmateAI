7ab1cf5d810cf0555ace1aa9b40079b8218117d9
Anton Sidliar <antonsidlyar@gmail.com>
2024-05-01 17:19:53 +0300

    Hello GroupmateAI

---
+++ src/test/java/com/asid/groupmateai/GroupmateAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asid.groupmateai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupmateAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
